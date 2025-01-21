32d5042b5633ad34a8869c6d08876e39a3bcf13f
Michael Haile <mhaile@student.42student.ae>
2025-01-22 03:52:16 +0400

    Feat: changed refreshtoken lifetime to 20, martin fixed AI smoothnesssssss

--- source/transcendence/transcendence/settings.py
+++ source/transcendence/transcendence/settings.py
@@ -216,7 +216,7 @@ AUTH_USER_MODEL = 'account.Player'
 
 # JWT settings
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME':  timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME':  timedelta(minutes=20),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
